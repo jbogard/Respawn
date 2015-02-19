e85d124527642ee8ef74e08a23ab7366513846af
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-02-19 10:30:02 -0600

    Nuget

---
+++ NuGetPack.bat
@@ -0,0 +1,1 @@
+.\.nuget\NuGet.exe pack .\Respawn\Respawn.csproj -Prop Configuration=Release
--- Respawn/Properties/AssemblyInfo.cs
+++ Respawn/Properties/AssemblyInfo.cs
@@ -1,26 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Respawn")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Intelligent checkpoints for database tests")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jimmy Bogard")]
 [assembly: AssemblyProduct("Respawn")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a326b923-6cab-48c7-82c7-8bb6780e2479")]
+[assembly: AssemblyCopyright("Copyright © Jimmy Bogard 2015")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +21,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: CLSCompliant(true)]
