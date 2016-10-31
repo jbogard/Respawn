d98138d169002c1df105d7e436899364e2d62519
Jimmy Bogard <jimmy.bogard@gmail.com>
2016-10-31 14:36:52 -0500

    0.2

--- Respawn/Properties/AssemblyInfo.cs
+++ Respawn/Properties/AssemblyInfo.cs
@@ -21,6 +21,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: CLSCompliant(true)]
