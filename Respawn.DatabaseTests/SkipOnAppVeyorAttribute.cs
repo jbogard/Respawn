6cdae29a3b218948db1127678355ccc84ceed106
Mickaël Derriey <mickael.derriey@readify.net>
2018-01-31 08:23:09 +1100

    Add an xUnit test discoverer that ignores tests when run on AppVeyor

---
+++ Respawn.DatabaseTests/SkipOnAppVeyorAttribute.cs
@@ -0,0 +1,34 @@
+﻿namespace Respawn.DatabaseTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Xunit;
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
+
+    public class SkipOnAppVeyorTestDiscoverer : IXunitTestCaseDiscoverer
+    {
+        private readonly IMessageSink _diagnosticMessageSink;
+
+        public SkipOnAppVeyorTestDiscoverer(IMessageSink diagnosticMessageSink)
+        {
+            _diagnosticMessageSink = diagnosticMessageSink;
+        }
+
+        public IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute)
+        {
+            if (Environment.GetEnvironmentVariable("Appveyor")?.ToUpperInvariant() == "TRUE")
+            {
+                return Enumerable.Empty<IXunitTestCase>();
+            }
+
+            return new[] { new XunitTestCase(_diagnosticMessageSink, discoveryOptions.MethodDisplayOrDefault(), testMethod) };
+        }
+    }
+
+    [XunitTestCaseDiscoverer("Respawn.DatabaseTests.SkipOnAppVeyorTestDiscoverer", "Respawn.DatabaseTests")]
+    public class SkipOnAppVeyorAttribute : FactAttribute
+    {
+    }
+}
